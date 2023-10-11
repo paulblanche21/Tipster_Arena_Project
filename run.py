2af461ffcdcda655a071dc268bb7905b7dfd2141
paulblanche21 <paulblanche21@gmail.com>
2023-10-11 15:40:29 +0100

    run changes

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 import os
+import os
+import sys
+from pathlib import Path
+
+# Adding the path to the Tipster_Arena package to the Python path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from Tipster_Arena.app import create_app, db, socketio  # Adjusted imports
 
 app = create_app()  # Creating an app instance
@@ -10,5 +17,6 @@ if __name__ == "__main__":
     if os.environ.get("FLASK_ENV") == "development":
         socketio.run(app, debug=True)
     else:
-        print("WARNING: Running in debug mode in a non-development environment")
+        print("WARNING: Running in debug mode in a"
+              + "non-development environment")
         socketio.run(app)
